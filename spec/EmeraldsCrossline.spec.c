f5e5dce792674272f05bee3e4bdd0984c966972c
oblivious <thanos.papap42@gmail.com>
2024-08-16 11:19:18 +0300

    Migrated to new cSpec.

--- spec/EmeraldsCrossline.spec.c
+++ spec/EmeraldsCrossline.spec.c
@@ -1,6 +1,6 @@
 #include "../libs/cSpec/export/cSpec.h"
 #include "crossline/crossline.module.spec.h"
 
-spec_suite({ T_crossline(); });
-
-int main(void) { run_spec_suite("all"); }
+int main(void) {
+  cspec_run_suite("all", { T_crossline(); });
+}
